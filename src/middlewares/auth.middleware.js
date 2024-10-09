dce0c8f833c19eb86baaf77333165103b2b2fdbf
rajan <rajandhamala0123@gmail.com>
2024-10-09 20:52:27 +0545

    custom middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,21 @@
 import {asyncHandeler} from '../utils/asyncHandeler.js'
-
+import {ApiError} from '../utils/ApiError.js'
+import jwwt from 'jsonwebtoken'
+import {User} from '../models/user.model.js'
 export const verifyJWt=asyncHandeler(async(req,res,next)=>{
-    req.cookkies?.accessToken || req.header("Authorization")?
+    const token=req.cookkies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+
+    if (!token) {
+        throw new ApiError(401, "Unouthorized request")
+    }
+
+    const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+
+    const user=await User.findById(decodedToken?.id).select("-password  -refreshToken")
+
+    id(!user){
+        throw new ApiError(401, "Invalid access token")
+    }
+    req.user=UserActivation;
+    next();
 })
