c9dfca314be5f3196e41371a043c91b9a217feb3
rajan <rajandhamala0123@gmail.com>
2024-10-23 22:53:52 +0545

    Add Vite and Tailwind CSS configurations

--- Another project/frontend/frontend/src/main.jsx
+++ Another project/frontend/frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Login from './Login'
+import Getdata from './Getdata'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Getdata />
   </StrictMode>,
 )
