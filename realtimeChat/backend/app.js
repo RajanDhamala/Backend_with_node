7469ffee17b1ac8714bda9c1f9a04c8691c478ae
rajan <rajandhamala0123@gmail.com>
2024-11-22 17:52:45 +0545

    databse connected and routes set

--- realtimeChat/backend/app.js
+++ realtimeChat/backend/app.js
@@ -1,4 +1,26 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import route from "./src/routes/user.routes.js"
 
 dotenv.config()
+
+const app=express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials:true
+    }
+))
+
+app.get("/rajan",(req,res)=>{
+    res.send('<h1>hello rajan bhai</h1>')
+})
+
+app.use("/api",route)
+
+
+export default app
