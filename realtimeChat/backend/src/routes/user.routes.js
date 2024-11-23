499f6734532d574a88fe701aa4263f8be894524b
rajan <rajandhamala0123@gmail.com>
2024-11-23 20:35:16 +0545

    user logged in and access and refresh token generated and sent in response

--- realtimeChat/backend/src/routes/user.routes.js
+++ realtimeChat/backend/src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import {registerUser} from '../controller/ManageUsr.js';
+import {registerUser,LoginUser} from '../controller/ManageUsr.js';
 const route=express.Router();
 
 route.post("/register",registerUser);
+route.post("/login",LoginUser);
 
 export default route
